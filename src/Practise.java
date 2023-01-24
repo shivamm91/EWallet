6459338b28247ef106e33ad3b3fe9a4ddc3661df
shivamm91 <shivamm91@gmail.com>
2023-01-24 23:46:36 +0530

    first commit

---
+++ src/Practise.java
@@ -0,0 +1,29 @@
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class Practise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		
+		
+	Stream<Integer> s1= Stream.of(45,54,12,34);
+	
+	Predicate<Integer> p1= t-> t>50;
+	s1.filter(t->t>50).sorted().forEach(t->System.out.println(t));
+	
+	
+	String s="ddfffrtrythfd";
+	Arrays.stream(s.split
+	
+	
+	
+			
+			
+	
+		
+	
+	}
+}
